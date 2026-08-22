13b98c3228aedbc55d3836342ce60be32c2083ba
辛磊 <xinleibird@gmail.com>
2026-08-22 20:25:44 +0800

    first commit

---
+++ TrinketMenuQueue.lua
@@ -0,0 +1,446 @@
+﻿--[[ TrinketMenuQueue : auto queue system ]]
+
+-- SavedVariable
+TrinketMenuQueue = {
+	Stats = {}, -- indexed by id of trinket, delay, priority and keep
+	Sort = {}, -- indexed by number, ids in order of use
+	Enabled = {} -- 0 or 1 whether auto queue is on for the slot
+}
+TrinketMenu.PausedQueue = {} -- 0 or 1 whether queue is paused
+
+function TrinketMenu.QueueInit()
+	TrinketMenuQueue.Sort[0] = TrinketMenuQueue.Sort[0] or {}
+	TrinketMenuQueue.Sort[1] = TrinketMenuQueue.Sort[1] or {}
+	TrinketMenu_SubQueueFrame:SetBackdropBorderColor(.3,.3,.3,1)
+	TrinketMenu_SortPriorityText:SetText("优先")
+	TrinketMenu_SortPriorityText:SetTextColor(.95,.95,.95)
+	TrinketMenu_SortKeepEquippedText:SetText("暂停自动排队")
+	TrinketMenu_SortKeepEquippedText:SetTextColor(.95,.95,.95)
+	TrinketMenu_SortListFrame:SetBackdropBorderColor(.3,.3,.3,1)
+	TrinketMenu.ReflectQueueEnabled()
+	TrinketMenu.UpdateCombatQueue()
+	TrinketMenu.BagsNeedUpdating = {}
+	TrinketMenu.CreateTimer("UpdateBaggedTrinkets",TrinketMenu.UpdateBaggedTrinkets,.2)
+	TrinketMenu_MainFrame:RegisterEvent("BAG_UPDATE")
+end
+
+function TrinketMenu.ReflectQueueEnabled()
+	TrinketMenu_Trinket0Check:SetChecked(TrinketMenuQueue.Enabled[0])
+	TrinketMenu_Trinket1Check:SetChecked(TrinketMenuQueue.Enabled[1])
+end	
+
+function TrinketMenu.OpenSort(which)
+	TrinketMenu.CurrentlySorting = which
+	TrinketMenu.PopulateSort(which)
+	TrinketMenu.SortSelected = 0
+	TrinketMenu_SortScrollScrollBar:SetValue(0)
+	TrinketMenu.SortValidate()
+	TrinketMenu.SortScrollFrameUpdate()
+end
+
+function TrinketMenu.GetID(bag,slot)
+	local id
+	if slot then
+		_,_,id = string.find(GetContainerItemLink(bag,slot) or "","item:(%d+)")
+	else
+		_,_,id = string.find(GetInventoryItemLink("player",bag) or "","item:(%d+)")
+	end
+	return id
+end
+
+function TrinketMenu.GetNameByID(id)
+	if id==0 then
+		return StopQueueHereText1,"Interface\\Buttons\\UI-GroupLoot-Pass-Up",1
+	else
+		local name,_,quality,_,_,_,_,_,texture = GetItemInfo(id or "")
+		return name,texture,quality
+	end
+end
+
+-- adds id to which sort if it's not already in the list
+function TrinketMenu.AddToSort(which,id)
+	if not id then return end
+	local name = TrinketMenu.GetNameByID(id)
+	if name and not TrinketMenu.WatchItem[name] then
+		TrinketMenu.AddWatchItem(name)
+	end
+
+	local found
+	for i=1,table.getn(TrinketMenuQueue.Sort[which]) do
+		found = found or TrinketMenuQueue.Sort[which][i]==id
+	end
+	if not found then
+		table.insert(TrinketMenuQueue.Sort[which],id)
+	end
+end
+
+-- populates sorts adding any new trinkets
+function TrinketMenu.PopulateSort(which)
+	TrinketMenuQueue.Sort[which] = TrinketMenuQueue.Sort[which] or {}
+	TrinketMenu.AddToSort(which,TrinketMenu.GetID(which+13))
+	TrinketMenu.AddToSort(which,TrinketMenu.GetID((1-which)+13))
+	local equipLoc,id
+	for i=0,4 do
+		for j=1,GetContainerNumSlots(i) do
+			id = TrinketMenu.GetID(i,j)
+			_,_,_,_,_,_,_,equipLoc = GetItemInfo(id or "")
+			if equipLoc=="INVTYPE_TRINKET" then
+				TrinketMenu.AddToSort(which,id)
+			end
+		end
+	end
+	TrinketMenu.AddToSort(which,0) -- id 0 is Stop
+end
+
+function TrinketMenu.SortScrollFrameUpdate()
+	local offset = FauxScrollFrame_GetOffset(TrinketMenu_SortScroll)
+	local list = TrinketMenuQueue.Sort[TrinketMenu.CurrentlySorting]
+	FauxScrollFrame_Update(TrinketMenu_SortScroll, list and table.getn(list) or 0, 9, 24)
+
+	if list then
+		local r,g,b,found
+		local texture,name,quality
+		local item,itemName,itemIcon
+		for i=1,9 do
+			item = getglobal("TrinketMenu_Sort"..i)
+			itemName = getglobal("TrinketMenu_Sort"..i.."Name")
+			itemIcon = getglobal("TrinketMenu_Sort"..i.."Icon")
+			idx = offset+i
+			if idx<=table.getn(list) then
+				name,texture,quality = TrinketMenu.GetNameByID(list[idx])
+				itemIcon:SetTexture(texture)
+				itemName:SetText(name)
+				r,g,b = GetItemQualityColor(quality)
+				itemName:SetTextColor(r,g,b)
+				itemIcon:SetVertexColor(1,1,1)
+				item:Show()
+				if idx == TrinketMenu.SortSelected then
+					TrinketMenu.LockHighlight(item)
+				else
+					TrinketMenu.UnlockHighlight(item)
+				end
+			else
+				item:Hide()
+			end
+		end
+	end
+end
+
+function TrinketMenu.LockHighlight(frame)
+	if type(frame)=="string" then frame = getglobal(frame) end
+	if not frame then return end
+	frame.lockedHighlight = 1
+	getglobal(frame:GetName().."Highlight"):Show()
+end
+
+function TrinketMenu.UnlockHighlight(frame)
+	if type(frame)=="string" then frame = getglobal(frame) end
+	if not frame then return end
+	frame.lockedHighlight = nil
+	getglobal(frame:GetName().."Highlight"):Hide()
+end
+
+-- shows tooltip for items in the sort list
+function TrinketMenu.SortTooltip()
+	local idx = FauxScrollFrame_GetOffset(TrinketMenu_SortScroll) + this:GetID()
+	local name,itemLink = GetItemInfo(TrinketMenuQueue.Sort[TrinketMenu.CurrentlySorting][idx] or "")
+	if itemLink and TrinketMenuOptions.ShowTooltips=="ON" then
+		TrinketMenu.AnchorTooltip()
+		GameTooltip:SetHyperlink(itemLink)
+		GameTooltip:Show()
+	else
+		TrinketMenu.OnTooltip(StopQueueHereText2,StopQueueHereTooltip)
+	end
+end
+
+function TrinketMenu.SortOnClick()
+	TrinketMenu_SortDelay:ClearFocus()
+	local idx = FauxScrollFrame_GetOffset(TrinketMenu_SortScroll) + this:GetID()
+	if TrinketMenu.SortSelected == idx then
+		TrinketMenu.SortSelected = 0
+	else
+		TrinketMenu.SortSelected = idx
+	end
+	TrinketMenu.SortScrollFrameUpdate()
+	TrinketMenu.SortValidate()
+end
+
+-- turns move buttons on/off, moves the list to keep selected in view and keeps the sorting slot button highlighted
+function TrinketMenu.SortValidate()
+	local selected = TrinketMenu.SortSelected
+	local list = TrinketMenuQueue.Sort[TrinketMenu.CurrentlySorting]
+	TrinketMenu_MoveTop:Enable()
+	TrinketMenu_MoveUp:Enable()
+	TrinketMenu_MoveDown:Enable()
+	TrinketMenu_MoveBottom:Enable()
+	if selected==0 or table.getn(list)<2 then -- none selected, disable all
+		TrinketMenu_MoveTop:Disable()
+		TrinketMenu_MoveUp:Disable()
+		TrinketMenu_MoveDown:Disable()
+		TrinketMenu_MoveBottom:Disable()
+	elseif selected==1 then -- top selected, disable up
+		TrinketMenu_MoveUp:Disable()
+		TrinketMenu_MoveTop:Disable()
+		TrinketMenu_MoveDown:Enable()
+	elseif selected == table.getn(list) then -- bottom selected, disable down
+		TrinketMenu_MoveDown:Disable()
+		TrinketMenu_MoveBottom:Disable()
+	end
+	local idx = FauxScrollFrame_GetOffset(TrinketMenu_SortScroll)
+	if selected>0 and list[selected] and list[selected]~=0 then
+		TrinketMenu_SortDelay:Show()
+		TrinketMenu_SortPriority:Show()
+		TrinketMenu_SortKeepEquipped:Show()
+	else
+		TrinketMenu_SortDelay:Hide()
+		TrinketMenu_SortPriority:Hide()
+		TrinketMenu_SortKeepEquipped:Hide()
+	end
+	local stats = TrinketMenuQueue.Stats[list[TrinketMenu.SortSelected]]
+	TrinketMenu_SortDelay:SetText(stats and (stats.delay or "0") or "0")
+	TrinketMenu_SortPriority:SetChecked(stats and stats.priority)
+	TrinketMenu_SortKeepEquipped:SetChecked(stats and stats.keep)
+			
+	if not IsShiftKeyDown() and selected>0 then -- keep selected visible on list, moving thumb as needed, unless shift is down
+		local parent = TrinketMenu_SortScrollScrollBar
+		local offset
+		if selected <= idx then
+			offset = (selected==1) and 0 or (parent:GetValue() - (parent:GetHeight() / 2))
+			parent:SetValue(offset)
+			PlaySound("UChatScrollButton")
+		elseif selected >= (idx+10) then
+			offset = (selected==table.getn(list)) and TrinketMenu_SortScroll:GetVerticalScrollRange() or (parent:GetValue() + (parent:GetHeight() / 2))
+			parent:SetValue(offset)
+			PlaySound("UChatScrollButton");
+		end
+	end
+end
+
+-- movement buttons
+function TrinketMenu.SortMove()
+	TrinketMenu_SortDelay:ClearFocus()
+	local dir = ((this==TrinketMenu_MoveUp) and -1) or ((this==TrinketMenu_MoveTop) and "top") or ((this==TrinketMenu_MoveDown) and 1) or ((this==TrinketMenu_MoveBottom) and "bottom")
+	if dir then
+		local idx1 = TrinketMenu.SortSelected -- FauxScrollFrame_GetOffset(ItemRack_Config_SortScroll) + 
+		local list = TrinketMenuQueue.Sort[TrinketMenu.CurrentlySorting]
+		local idx2 = ((dir=="top") and 1) or ((dir=="bottom") and table.getn(list)) or idx1+dir
+		local temp = list[idx1]
+		if tonumber(dir) then
+			list[idx1] = list[idx2]
+			list[idx2] = temp
+		elseif dir=="top" then
+			table.remove(list,idx1)
+			table.insert(list,1,temp)
+		elseif dir=="bottom" then
+			table.remove(list,idx1)
+			table.insert(list,temp)
+		end
+		TrinketMenu.SortSelected = idx2
+		TrinketMenu.SortValidate()
+		TrinketMenu.SortScrollFrameUpdate()
+	end
+end
+
+function TrinketMenu.SortDelay_OnTextChanged()
+	local delay = tonumber(TrinketMenu_SortDelay:GetText()) or 0
+	local id = TrinketMenuQueue.Sort[TrinketMenu.CurrentlySorting][TrinketMenu.SortSelected]
+	TrinketMenuQueue.Stats[id] = TrinketMenuQueue.Stats[id] or {}
+	TrinketMenuQueue.Stats[id].delay = delay~=0 and delay or nil
+end
+
+function TrinketMenu.SortPriority_OnClick()
+	local check = this:GetChecked()
+	local id = TrinketMenuQueue.Sort[TrinketMenu.CurrentlySorting][TrinketMenu.SortSelected]
+	TrinketMenuQueue.Stats[id] = TrinketMenuQueue.Stats[id] or {}
+	TrinketMenuQueue.Stats[id].priority = check
+end
+
+function TrinketMenu.SortKeepEquipped_OnClick()
+	local check = this:GetChecked()
+	local id = TrinketMenuQueue.Sort[TrinketMenu.CurrentlySorting][TrinketMenu.SortSelected]
+	TrinketMenuQueue.Stats[id] = TrinketMenuQueue.Stats[id] or {}
+	TrinketMenuQueue.Stats[id].keep = check
+end
+
+function TrinketMenu.TabCheck_OnClick()
+	TrinketMenuQueue.Enabled[3-this:GetID()] = this:GetChecked()
+	TrinketMenu.UpdateCombatQueue()
+end
+
+--[[ Auto queue processing ]]
+
+function TrinketMenu.UpdateBaggedTrinkets()
+	local id,name,equipLoc
+	for i in TrinketMenu.BagsNeedUpdating do
+		for j=1,GetContainerNumSlots(i) do
+			_,_,id = string.find(GetContainerItemLink(i,j) or "","item:(%d+)")
+			name,_,_,_,_,_,_,equipLoc = GetItemInfo(id or "")
+			if equipLoc=="INVTYPE_TRINKET" then
+				TrinketMenu.AddWatchItem(name,nil,i,j)
+			end
+		end
+		TrinketMenu.BagsNeedUpdating[i] = nil
+	end
+end
+
+function TrinketMenu.TrinketNearReady(bag,slot)
+	local start,duration
+	if slot then
+		start,duration = GetContainerItemCooldown(bag,slot)
+	else
+		start,duration = GetInventoryItemCooldown("player",bag)
+	end
+	if start==0 or duration-(GetTime()-start)<=30 then
+		return 1
+	end
+end
+
+function TrinketMenu.CanCooldown(inv)
+	local _,_,enable = GetInventoryItemCooldown("player",inv)
+	return enable==1
+end
+
+-- this function quickly checks if conditions are right for a possible ProcessAutoQueue
+function TrinketMenu.PeriodicQueueCheck()
+	for i=0,1 do
+		if TrinketMenuQueue.Enabled[i] then
+			TrinketMenu.ProcessAutoQueue(i)
+		end
+	end
+end
+
+-- which = 0 or 1, decides if a trinket should be equipped and equips if so
+function TrinketMenu.ProcessAutoQueue(which)
+
+	local start,duration,enable = GetInventoryItemCooldown("player",13+which)
+	local _,_,id,name = string.find(GetInventoryItemLink("player",13+which) or "","item:(%d+).+%[(.+)%]")
+	local icon = getglobal("TrinketMenu_Trinket"..which.."Queue") 
+
+	if not id then return end -- leave if no trinket equipped
+	if IsInventoryItemLocked(13+which) then return end -- leave if slot being swapped
+	if TrinketMenu.PausedQueue[which] then
+		icon:SetVertexColor(1,.5,.5) -- leave if SetQueue(which,"PAUSE")
+		return
+	end
+	if TrinketMenuQueue.Stats[id] then
+		if TrinketMenuQueue.Stats[id].keep then
+			icon:SetVertexColor(1,.5,.5)
+			return -- leave if .keep flag set on this item
+		end
+		if TrinketMenuQueue.Stats[id].delay then
+			local timeLeft = GetTime()-start
+			-- leave if currently equipped trinket is on cooldown for less than its delay
+			if start>0 and (duration-timeLeft)>30 and timeLeft<TrinketMenuQueue.Stats[id].delay then
+				icon:SetDesaturated(1)
+				return
+			end
+		end
+	end
+
+	icon:SetDesaturated(0) -- normal queue operation, reflect that in queue inset
+	icon:SetVertexColor(1,1,1)
+
+--	local name = TrinketMenu.GetNameByID(id)
+	local ready = TrinketMenu.TrinketNearReady(13+which)
+	if ready and TrinketMenu.CombatQueue[which] then
+		TrinketMenu.CombatQueue[which] = nil
+		TrinketMenu.UpdateCombatQueue()
+	end
+	local list,rank = TrinketMenuQueue.Sort[which]
+	for i=1,table.getn(list) do
+		if list[i]==0 then rank=i break end
+		if ready and list[i]==id then rank=i break end
+	end
+	if rank then
+		local bag,slot
+		for i=1,rank do
+			if not ready or enable==0 or (TrinketMenuQueue.Stats[list[i]] and TrinketMenuQueue.Stats[list[i]].priority) then
+				name = GetItemInfo(list[i]) or ""
+				if TrinketMenu.WatchItem[name] then
+					bag,slot = TrinketMenu.WatchItem[name].bag,TrinketMenu.WatchItem[name].slot
+					if bag then
+						if string.find(GetContainerItemLink(bag,slot) or "",name,1,1) then
+							if TrinketMenu.TrinketNearReady(bag,slot) then
+								if TrinketMenu.CombatQueue[which]~=name then
+									TrinketMenu.EquipTrinketByName(name,13+which)
+								end
+								break
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+--[[ TrinketMenu.SetQueue and TrinketMenu.GetQueue ]]
+
+-- These functions are for macros and mods to configure sort queues.
+
+-- TrinketMenu.SetQueue(0 or 1,"ON" or "OFF" or "PAUSE" or "RESUME" or "SORT"[,"sort list")
+-- some examples:
+-- TrinketMenu.SetQueue(1,"PAUSE") -- if queue is going, pause it
+-- TrinketMenu.SetQueue(1,"RESUME") -- if queue is paused, resume it
+-- TrinketMenu.SetQueue(1,"SORT","Earthstrike","Insignia of the Alliance","Diamond Flask") -- set sort
+-- TrinketMenu.SetQUeue(0,"SORT","Lifestone","Darkmoon Card: Heroism") -- set sort for trinket 0
+-- (a "stop the queue" is assumed at the end of the list)
+function TrinketMenu.SetQueue(which,...)
+	local errorstub = "|cFFBBBBBBTrinketMenu.SetQueue:|cFFFFFFFF "
+	if not which or not tonumber(which) or which<0 or which>1 then
+		DEFAULT_CHAT_FRAME:AddMessage(errorstub.."First parameter must be 0 for top trinket or 1 for bottom.")
+		return
+	end
+	if table.getn(arg)<1 then
+		DEFAULT_CHAT_FRAME:AddMessage(errorstub.."Second parameter is either ON, OFF, PAUSE, RESUME or the beginning of a list of trinkets in a sort order.")
+		return
+	end
+	if TrinketMenu_OptFrame:IsVisible() then
+		TrinketMenu_OptFrame:Hide() -- close option frame if it's up. the mess otherwise would be scary
+	end
+	if arg[1]=="ON" then
+		TrinketMenuQueue.Enabled[which]=1
+		TrinketMenu.PausedQueue[which]=nil
+	elseif arg[1]=="OFF" then
+		TrinketMenuQueue.Enabled[which]=nil
+		TrinketMenu.PausedQueue[which]=nil
+	elseif arg[1]=="PAUSE" then
+		TrinketMenu.PausedQueue[which]=1
+	elseif arg[1]=="RESUME" then
+		TrinketMenu.PausedQueue[which]=nil
+	elseif arg[1]=="SORT" and table.getn(arg)>1 then
+		local sortidx,inv,bag,slot,id = 1
+		table.setn(TrinketMenuQueue.Sort[which],0)
+		for i=2,table.getn(arg) do
+			inv,bag,slot = TrinketMenu.FindItem(arg[i],1) -- include inventory
+			if inv then
+				table.insert(TrinketMenuQueue.Sort[which],TrinketMenu.GetID(inv))
+			elseif bag then
+				table.insert(TrinketMenuQueue.Sort[which],TrinketMenu.GetID(bag,slot))
+			else
+				DEFAULT_CHAT_FRAME:AddMessage(errorstub.."Trinket \""..arg[i].."\" not found.")
+			end
+		end
+		table.insert(TrinketMenuQueue.Sort[which],0)
+	else
+		DEFAULT_CHAT_FRAME:AddMessage(errorstub.." Expected ON, OFF, PAUSE, RESUME or SORT+list")
+	end
+
+	TrinketMenu.ReflectQueueEnabled()
+	TrinketMenu.UpdateCombatQueue()
+
+end
+
+-- returns 1 or nil if queue is enabled, and a table containing an ordered list of the trinkets
+function TrinketMenu.GetQueue(which)
+	if not which or not tonumber(which) or which<0 or which>1 then
+		DEFAULT_CHAT_FRAME:AddMessage("|cFFBBBBBBTrinketMenu.GetQueue:|cFFFFFFFF Parameter must be 0 for top trinket or 1 for bottom.")
+		return
+	end
+	local trinketList,name = {}
+	for i=1,table.getn(TrinketMenuQueue.Sort[which]) do
+		name = TrinketMenu.GetNameByID(TrinketMenuQueue.Sort[which][i])
+		table.insert(trinketList,name)
+	end
+	return TrinketMenuQueue.Enabled[which],trinketList
+end
---
+++ localization.lua
@@ -0,0 +1,77 @@
+﻿TrinketMenu.Tooltip1 ="Click: toggle options\nDrag: move icon";
+TrinketMenu.Tooltip1 = "Left click: toggle trinkets\nRight click: toggle options\nDrag: move icon";
+StopQueueHereText1 = "-- stop queue here --";
+StopQueueHereText2 = "Stop Queue Here";
+StopQueueHereTooltip = "Move this to mark the lowest trinket to auto queue.  Sometimes you may want a passive trinket with a click effect to be the end (Burst of Knowledge, Second Wind, etc).";
+
+TrinketMenu.Message1 = "|cFFFFFF00TrinketMenu scale:";
+TrinketMenu.Message2 = "/trinket scale main (number) : set exact main scale";
+TrinketMenu.Message3 = "/trinket scale menu (number) : set exact menu scale";
+TrinketMenu.Message4 = "ie, /trinket scale menu 0.85";
+TrinketMenu.Message5 = "Note: You can drag the lower-right corner of either window to scale.  This slash command is for those who want to set an exact scale.";
+TrinketMenu.Message6= "|cFFFFFF00TrinketMenu useage:";
+TrinketMenu.Message7 = "/trinket or /trinketmenu : toggle the window";
+TrinketMenu.Message8 = "/trinket reset : reset all settings";
+TrinketMenu.Message9 = "/trinket opt : summon options window";
+TrinketMenu.Message10 = "/trinket lock|unlock : toggles window lock";
+TrinketMenu.Message11 = "/trinket scale main|menu (number) : sets an exact scale";
+
+if (GetLocale() == "zhCN") then
+TrinketMenu.CheckOptInfo = {
+	{"ShowIcon","ON","迷你地图按钮","显示或隐藏小地图按钮."},
+	{"SquareMinimap","OFF","方形迷你地图","如果迷你地图是方形移动迷你地图按钮.","ShowIcon"},
+	{"CooldownCount","OFF","冷却计时","在按钮上显示剩余冷却时间."},
+	{"TooltipFollow","OFF","跟随鼠标","提示信息跟随鼠标.","ShowTooltips"},
+	{"KeepOpen","OFF","保持列表开启","保持饰物列表始终开启."},
+	{"KeepDocked","ON","保持列表粘附","保持饰物列表粘附在当前装备列表."},
+	{"Notify","OFF","可使用提示","饰物冷却后提示玩家."},
+	{"DisableToggle","OFF","禁止开关","止使用点击小地图按钮来控制列表的显示/隐藏.","ShowIcon"},
+	{"NotifyChatAlso","OFF","在聊天窗口提示","在饰物冷却结束后在聊天窗口也发出提示信息."},
+	{"Locked","OFF","锁定窗口","不能移动,缩放,转动饰品列表."},
+	{"ShowTooltips","ON","显示提示信息","显示提示信息."},
+	{"NotifyThirty","ON","三十秒提示","在饰品冷却前三十秒时提示玩家."},
+	{"MenuOnShift","OFF","Shift显示列表","只有按下Shift才会显示饰品选择列表."},
+	{"TinyTooltips","OFF","迷你提示","简化饰品的提示信息变为只有名字, 用途, 冷却.","ShowTooltips"},
+	{"SetColumns","OFF","设置列表列数","设置饰品选择列表的列数.\n\n不选择此项 TrinketMenu 会自动排列."},
+	{"LargeCooldown","ON","大字体","用更大的字体显示冷却时间.","CooldownCount"},
+	{"ShowHotKeys","ON","显示快捷键","在饰品上显示绑定的快捷键."},
+	{"StopOnSwap","OFF","被动饰品停止排队","当换上一个被动饰品时停止自动排队.  选中这个选项时, 当一个可点击饰品通过 TrinketMenu 被手动换上时同样会停止自动排队.."}
+}
+
+TrinketMenu.TooltipInfo = {
+	{"TrinketMenu_LockButton","锁定窗口","不能移动,缩放,转动饰品列表."},
+	{"TrinketMenu_Trinket0Check","上面饰品栏自动排队","选中这个选项会让饰品自动排队替换到上面的饰品栏.  你也可以Alt+点击饰品来开关自动排队."},
+	{"TrinketMenu_Trinket1Check","下面饰品栏自动排队","选中这个选项会让饰品自动排队替换到下面的饰品栏.  你也可以Alt+点击饰品来开关自动排队."},
+	{"TrinketMenu_SortPriority","高优先权","当选中这个选项时, 这个饰品会被第一时间装备上, 而不管装备着的饰品是否在冷却中.\n\n当没选中时, 这个饰品不会替换掉没有在冷却中的已装备饰品."},
+	{"TrinketMenu_SortDelay","延迟替换","设置一个饰品被替换的时间 (秒).  比如, 你需要20秒得到大地之击的20秒BUFF."},
+	{"TrinketMenu_SortKeepEquipped","暂停自动排队","选中这个选项,当这个饰品被装备时会暂停自动排队替换. 比如, 你有一个自动换装的插件在你骑马时把棍子上的胡萝卜装备上了."}
+}
+
+TrinketMenu.Tooltip1 = "点击: 开关选项窗口\n拖动: 移动设置按钮";
+TrinketMenu.Tooltip2 = "左键点击: 开关饰物窗口\n右键点击: 开关选项窗口\n拖动: 移动设置按钮";
+StopQueueHereText1 = "-- 停止排队线 --";
+StopQueueHereText2 = "停止排队线";
+StopQueueHereTooltip = "移动这个来标记让下面的饰品不自动排队.  当你想让一个被动饰品只允许手动换上时, 就把它移动到这条线的下面.";
+
+TrinketMenu_Tab1:SetText("选项");
+TrinketMenu_Tab2:SetText("下面      ");
+TrinketMenu_Tab3:SetText("上面      ");
+
+TrinketMenu_SortDelayText1:SetText("延迟")
+TrinketMenu_SortDelayText2:SetText("秒")
+--TrinketMenu_SortPriorityText:SetText("优先")
+--TrinketMenu_SortKeepEquippedText:SetText("暂停自动排队")
+
+TrinketMenu.Message1 = "|cFFFFFF00TrinketMenu 缩放:";
+TrinketMenu.Message2 = "/trinket scale main (number) : 设置主列表缩放比";
+TrinketMenu.Message3 = "/trinket scale menu (number) : 设置选择列表缩放比";
+TrinketMenu.Message4 = "比如: /trinket scale menu 0.85";
+TrinketMenu.Message5 = "提示: 你可以拖动右下角调整缩放比.";
+TrinketMenu.Message6= "|cFFFFFF00TrinketMenu 帮助:";
+TrinketMenu.Message7 = "/trinket or /trinketmenu : 开关列表";
+TrinketMenu.Message8 = "/trinket reset : 重置所有设置";
+TrinketMenu.Message9 = "/trinket opt : 打开设置窗口";
+TrinketMenu.Message10 = "/trinket lock|unlock : 锁定/解锁窗口";
+TrinketMenu.Message11 = "/trinket scale main|menu (number) : 缩放主/列表";
+
+end
